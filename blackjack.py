b3c2dd735259075494d29b98bbfbb64aad3df9ae
Virgile Cheminot <virgile.cheminot@gmail.com>
2021-10-22 21:05:39 +0200

    create main files

---
+++ blackjack.py
@@ -0,0 +1,21 @@
+## PAQUET DE CARTES ##
+
+from random import *
+
+cartes = {
+    'as' : [1, 11],
+    '2' : 2,
+    '3' : 3,
+    '4' : 4,
+    '5' : 5,
+    '6' : 6,
+    '7' : 7,
+    '8' : 8,
+    '9' : 9,
+    '10' : 10,
+    'valet' : 10,
+    'dame' : 10,
+    'roi' : 10
+}
+
+def paquet():
---
+++ main.py
@@ -0,0 +1,2 @@
+import blackjack
+blackjack.test()
